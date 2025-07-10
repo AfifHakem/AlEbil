ab96639c3b3345c597b7e1763c47165948ffd45c
AfifHakem <m-11161992@moe-dl.edu.my>
2025-07-10 20:43:09 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0f766e", // hijau
+        secondary: "#facc15", // emas
+      },
+    },
+  },
+  plugins: [],
+};
